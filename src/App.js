8b0f9feab329d83f0aecf95b9f49559ae984087e
Naoaki Takado <67215204+naoakitakado@users.noreply.github.com>
2022-05-05 09:25:54 +0900

    7

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
   console.log("App");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState("");
+
+  const onChangeText = (e) => setText(e.target.value);
+
   const onClickCountUp = () => {
     setCount(count + 1);
   };
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button>表示</button>
+      <ChildArea />
     </div>
   );
 }
